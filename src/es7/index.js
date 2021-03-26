7e02cf2a06a6f896158b095cbcfbd4bd2b3e9df7
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-25 23:40:47 -0600

    Includes and exponents

---
+++ src/es7/index.js
@@ -0,0 +1,14 @@
+// Includes, ver si hay un elemento dentro de un arreglo
+let numbers = [1,2,3,7,8];
+
+if(numbers.includes(9)){
+    console.log('Si se encuentra el valor 7');
+} else{
+    console.log("No se encuentra");
+}
+
+//Elevar a la potencia
+let base = 4;
+let exponent = 3;
+var result = base ** exponent;
+console.log(result);
