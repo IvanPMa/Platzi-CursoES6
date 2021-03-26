6cce48fad3b183350b15291220a9ca2ad2538410
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-26 12:37:43 -0600

    Async -  await

--- src/es8/index.js
+++ src/es8/index.js
@@ -31,3 +31,34 @@ const obje = {
     name : 'Ivan',
 }
 
+// Async await
+
+const helloWorld = ()=>{
+    return new Promise((resolve, reject) =>{
+        (true)
+            ? setTimeout(() => resolve ('Hello Wolrd'), 3000 ) 
+            : reject (new Error('Test Error'))
+    });
+};
+
+const  helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+// -ejecutar correctamente 
+
+const anotherFunction = async () =>{
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    }
+
+    catch (error){
+        console.log(error);
+    }
+};
+
+anotherFunction();
